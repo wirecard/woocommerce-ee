b17d096604f8ac73675bcf376e1cf18d1bf7b367
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2018-01-15 11:15:51 +0100

    Add bootstrap and default plugin file

---
+++ wc-wirecard-ee/tests/test-wc-wirecard-ee.php
@@ -0,0 +1,18 @@
+
+<?php
+/**
+ * Class WcWirecardEETest
+ *
+ * @package WC_Wirecard_EE
+ */
+/**
+ * WcWirecardEE test case.
+ */
+class WcWirecardEETest extends WP_UnitTestCase {
+    /**
+     * Default test.
+     */
+    function test_wc_wirecard_ee() {
+        $this->assertTrue( true );
+    }
+}
