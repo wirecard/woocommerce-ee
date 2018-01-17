43fff4123827d97125a8447bc7295c84e7faaa52
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2018-01-17 14:35:54 +0100

    #1 Add default test file

---
+++ wc-wirecard-ee/Test/test-woocommerce-wirecard-ee.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Default test case.
+ */
+class Wirecard_Payment_Gateway_Test extends \PHPUnit_Framework_TestCase {
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
