3747a47530f571ce2fd7d256db10f47a3f4ce963
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2018-04-16 15:23:41 +0200

    #37 Implement Unionpay International class

---
+++ woocommerce-wirecard-ee/classes/includes/class-wc-gateway-wirecard-unionpay-international.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+require_once __DIR__ . '/class-wc-wirecard-payment-gateway.php';
+
+use Wirecard\PaymentSdk\Config\Config;
+use Wirecard\PaymentSdk\Config\PaymentMethodConfig;
+
+/**
+ * Class WC_Gateway_Wirecard_Unionpay_International
+ *
+ * @extends WC_Wirecard_Payment_Gateway
+ *
+ * @since   1.1.0
+ */
+class WC_Gateway_Wirecard_Unionpay_International extends WC_Wirecard_Payment_Gateway {
+
+	/**
+	 * WC_Gateway_Wirecard_Unionpay_International constructor.
+	 *
+	 * @since 1.1.0
+	 */
+	public function __construct() {
+		$this->type               = 'unionpayinternational';
+		$this->id                 = 'wirecard_ee_unionpayinternational';
+		$this->icon               = WOOCOMMERCE_GATEWAY_WIRECARD_URL . 'assets/images/unionpayinternational.png';
+		$this->method_title       = __( 'Wirecard Unionpay International', 'wooocommerce-gateway-wirecard' );
+		$this->method_name        = __( 'Unionpay International', 'wooocommerce-gateway-wirecard' );
+		$this->method_description = __( 'Unionpay International transactions via Wirecard Payment Processing Gateway', 'woocommerce-gateway-wirecard' );
+		$this->has_fields         = true;
+
+		$this->supports = array(
+			'products',
+			'refunds',
+		);
+
+		$this->cancel        = array( 'authorization' );
+		$this->capture       = array( 'authorization' );
+		$this->refund        = array( 'purchase', 'capture-authorization' );
+		$this->refund_action = 'refund';
+
+		$this->init_form_fields();
+		$this->init_settings();
+
+		$this->title   = $this->get_option( 'title' );
+		$this->enabled = $this->get_option( 'enabled' );
+
+		$this->additional_helper = new Additional_Information();
+
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		add_action( 'woocommerce_api_get_credit_card_request_data', array( $this, 'get_request_data' ) );
+
+		parent::add_payment_gateway_actions();
+	}
+
+	/**
+	 * Load form fields for configuration
+	 *
+	 * @since 1.1.0
+	 */
+	public function init_form_fields() {
+		$this->form_fields = array(
+			'enabled'                     => array(
+				'title'   => __( 'Enable/Disable', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Enable Wirecard Unionpay International', 'woocommerce-gateway-wirecard' ),
+				'default' => 'yes',
+			),
+			'title'                       => array(
+				'title'       => __( 'Title', 'woocommerce-gateway-wirecard' ),
+				'type'        => 'text',
+				'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce-gateway-wirecard' ),
+				'default'     => __( 'Wirecard Unionpay International', 'woocommerce-gateway-wirecard' ),
+				'desc_tip'    => true,
+			),
+			'merchant_account_id'         => array(
+				'title'   => __( 'Merchant Account ID', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'text',
+				'default' => 'c6e9331c-5c1f-4fc6-8a08-ef65ce09ddb0',
+			),
+			'secret'                      => array(
+				'title'   => __( 'Secret Key', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'text',
+				'default' => '16d85b73-79e2-4c33-932a-7da99fb04a9c',
+			),
+			'credentials'                 => array(
+				'title'       => __( 'Credentials', 'woocommerce-gateway-wirecard' ),
+				'type'        => 'title',
+				'description' => __( 'Enter your Wirecard credentials.', 'woocommerce-gateway-wirecard' ),
+			),
+			'base_url'                    => array(
+				'title'       => __( 'Base URL', 'woocommerce-gateway-wirecard' ),
+				'type'        => 'text',
+				'description' => __( 'The Wirecard base URL. (e.g. https://api.wirecard.com)' ),
+				'default'     => 'https://api-test.wirecard.com',
+				'desc_tip'    => true,
+			),
+			'http_user'                   => array(
+				'title'   => __( 'HTTP User', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'text',
+				'default' => '70000-APILUHN-CARD',
+			),
+			'http_pass'                   => array(
+				'title'   => __( 'HTTP Password', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'text',
+				'default' => '8mhwavKVb91T',
+			),
+			'advanced'                    => array(
+				'title'       => __( 'Advanced Options', 'woocommerce-gateway-wirecard' ),
+				'type'        => 'title',
+				'description' => '',
+			),
+			'payment_action'              => array(
+				'title'   => __( 'Payment Action', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'select',
+				'default' => 'Capture',
+				'label'   => __( 'Payment Action', 'woocommerce-gateway-wirecard' ),
+				'options' => array(
+					'reserve' => 'Authorization',
+					'pay'     => 'Capture',
+				),
+			),
+			'descriptor'                  => array(
+				'title'   => __( 'Enable/Disable', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Descriptor', 'woocommerce-gateway-wirecard' ),
+				'default' => 'no',
+			),
+			'send_additional'             => array(
+				'title'   => __( 'Enable/Disable', 'woocommerce-gateway-wirecard' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Send additional information', 'woocommerce-gateway-wirecard' ),
+				'default' => 'yes',
+			),
+		);
+	}
+
+	/**
+	 * Create payment method Configuration
+	 *
+	 * @return Config
+	 *
+	 * @since 1.1.0
+	 */
+	public function create_payment_config( $base_url = null, $http_user = null, $http_pass = null ) {
+		if ( is_null( $base_url ) ) {
+			$base_url  = $this->get_option( 'base_url' );
+			$http_user = $this->get_option( 'http_user' );
+			$http_pass = $this->get_option( 'http_pass' );
+		}
+
+		$config         = parent::create_payment_config( $base_url, $http_user, $http_pass );
+		$payment_config = new PaymentMethodConfig(
+			\Wirecard\PaymentSdk\Transaction\UpiTransaction::NAME,
+			$this->get_option( 'merchant_account_id' ),
+			$this->get_option( 'secret' )
+		);
+
+		$config->add( $payment_config );
+
+		return $config;
+	}
+}
