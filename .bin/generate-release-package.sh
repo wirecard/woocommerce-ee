745f66ec5a1f5fa6db312fbb0c284d1ac835159f
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-08-24 11:32:28 +0200

    Update package name

--- .bin/generate-release-package.sh
+++ .bin/generate-release-package.sh
@@ -4,4 +4,4 @@ TARGET_DIRECTORY="wirecard-woocommerce-extension"
 
 composer install --no-dev
 
-zip -r woocommerce-ee.zip ${TARGET_DIRECTORY}
+zip -r woocommerce-wirecard-ee.zip ${TARGET_DIRECTORY}
