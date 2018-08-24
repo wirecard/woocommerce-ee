836447df657f839747a84ee926786f71843e0b05
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-08-24 10:56:27 +0200

    Update release package script

--- .bin/generate-release-package.sh
+++ .bin/generate-release-package.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-TARGET_DIRECTORY="upload"
+TARGET_DIRECTORY="wirecard-woocommerce-extension"
 
 composer install --no-dev
-rm -rf $TARGET_DIRECTORY
-echo "copying files to target directory ${TARGET_DIRECTORY}"
-mkdir $TARGET_DIRECTORY
-cp -r wirecard-woocommerce-extension ${TARGET_DIRECTORY}/
 
-zip -r wocommerce-ee.zip ${TARGET_DIRECTORY} install.xml
+zip -r wocommerce-ee.zip ${TARGET_DIRECTORY}
