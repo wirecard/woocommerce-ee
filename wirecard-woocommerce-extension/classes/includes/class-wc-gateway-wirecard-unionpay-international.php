08aafe19908d38e10423a2b3aaed5c084df5a116
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-09-20 14:13:39 +0200

    #91 Update base url

--- wirecard-woocommerce-extension/classes/includes/class-wc-gateway-wirecard-unionpay-international.php
+++ wirecard-woocommerce-extension/classes/includes/class-wc-gateway-wirecard-unionpay-international.php
@@ -233,7 +233,7 @@ HTML;
 		$transaction_service = new TransactionService( $config );
 		$lang                = 'en';
 		try {
-			$supported_lang = json_decode( file_get_contents( 'https://api-test.wirecard.com/engine/includes/i18n/languages/hpplanguages.json' ) );
+			$supported_lang = json_decode( file_get_contents( $this->get_option( 'base_url' ) . '/engine/includes/i18n/languages/hpplanguages.json' ) );
 			if ( key_exists( substr( get_locale(), 0, 2 ), $supported_lang ) ) {
 				$lang = substr( get_locale(), 0, 2 );
 			}
