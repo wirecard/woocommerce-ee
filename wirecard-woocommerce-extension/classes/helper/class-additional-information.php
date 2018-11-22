8535187e581d4f99b8d712119fa7cfbefe627d49
Tomaž Puhar <tomaz.puhar@wirecard.com>
2018-11-22 14:22:51 +0100

    #121 Update error message

--- wirecard-woocommerce-extension/classes/helper/class-additional-information.php
+++ wirecard-woocommerce-extension/classes/helper/class-additional-information.php
@@ -346,7 +346,7 @@ class Additional_Information {
 					$shipping['tax-rate']
 				);
 			} else {
-				return new WP_Error( 'error', __( 'You can only refund full shipping or none :D muhahahahaha.', 'wirecard-woocommerce-extension' ) );
+				return new WP_Error( 'error', __( 'You are only able to refund the whole shipping, partial shipping refunds are not possible.', 'wirecard-woocommerce-extension' ) );
 			}
 		}
 
