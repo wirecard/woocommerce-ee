42663fe0cbc12803199e888c2ae30e86d802888e
Tomaž Puhar <tomaz.puhar@wirecard.com>
2018-11-30 10:46:28 +0100

    #128 Update livechat url

--- wirecard-woocommerce-extension/woocommerce-wirecard-payment-gateway.php
+++ wirecard-woocommerce-extension/woocommerce-wirecard-payment-gateway.php
@@ -279,7 +279,7 @@ function wirecard_gateway_options_page() {
  * @since 1.1.5
  */
 function backend_scripts() {
-	wp_register_script( 'live_chat', 'http://www.provusgroup.com/livezilla/script.php?id=936f87cd4ce16e1e60bea40b45b0596a', array(), null, true );
+	wp_register_script( 'live_chat', 'https://www.provusgroup.com/livezilla/script.php?id=936f87cd4ce16e1e60bea40b45b0596a', array(), null, true );
 	wp_register_script( 'plugin_admin_script', WIRECARD_EXTENSION_URL . 'assets/js/admin/plugin_admin.js', array(), null, false );
 }
 
