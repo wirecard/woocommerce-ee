441b753d99f6fe3f446e4cbea9bbde01da88848b
Nikolaos Stamatopoulos <stamatopoulos@parkside.at>
2018-12-16 17:00:08 +0100

    Update constants

--- .bin/phraseapp/const.rb
+++ .bin/phraseapp/const.rb
@@ -1,5 +1,8 @@
 module Const
-  GITHUB_PHRASEAPP_PR_TITLE = '[PhraseApp] Update translations'.freeze
+  GITHUB_PHRASEAPP_PR_TITLE = '[PhraseApp] Update locales'.freeze
+  GITHUB_PHRASEAPP_PR_BODY = 'Update locales from PhraseApp'.freeze
+  GIT_PHRASEAPP_COMMIT_MSG = '[skip ci] Update translations from PhraseApp'.freeze
+  GIT_PHRASEAPP_BRANCH_BASE = 'master'.freeze
   PHRASEAPP_PROJECT_ID = '9036e89959d471e0c2543431713b7ba1'.freeze
   PHRASEAPP_FALLBACK_LOCALE = 'en_US'.freeze
 
@@ -11,5 +14,6 @@ module Const
   }.freeze
 
   # paths relative to project root
-  PLUGIN_I18N_DIR = 'wirecard-woocommerce-extension/languages'.freeze
+  PLUGIN_DIR = 'wirecard-woocommerce-extension'.freeze
+  PLUGIN_I18N_DIR = File.join(PLUGIN_DIR, 'languages').freeze
 end
