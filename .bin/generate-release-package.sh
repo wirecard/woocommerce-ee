f768f3b83db6e0e0533c56fd0154b62ef5e6a046
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2019-01-02 15:35:57 +0100

    #164 Keep composer.json file in release package for acceptance tests

--- .bin/generate-release-package.sh
+++ .bin/generate-release-package.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# This script will generate release package and keep composer.json if 'test' parameter is passed
 
 TARGET_DIRECTORY="wirecard-woocommerce-extension"
 
 composer install --no-dev
+if [[ $1 == 'test' ]]; then
+    zip -r woocommerce-wirecard-ee.zip ${TARGET_DIRECTORY} composer.json -x "*tests*" -x "*Test*"
+else
+    zip -r woocommerce-wirecard-ee.zip ${TARGET_DIRECTORY} -x "*tests*" -x "*Test*"
+fi
 
-zip -r woocommerce-wirecard-ee.zip ${TARGET_DIRECTORY} -x "*tests*" -x "*Test*"
