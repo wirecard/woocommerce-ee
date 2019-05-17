a883b12eb1c4c228ac7f0833fcb33e51a88d474b
brkicadis <adis.brkic@wirecard.com>
2019-05-17 12:06:13 +0200

    Test new release on different versions

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -16,7 +16,7 @@ fi
 
 #send information about the build
 curl -X POST -H 'Content-type: application/json' \
-    --data "{'text': 'Build Failed. Woocommerce version: ${SHOP_RELEASE}\n
+    --data "{'text': 'Build Failed. Woocommerce version: ${WOOCOMMERCE_VERSION}\n
     Build URL : ${TRAVIS_JOB_WEB_URL}\n
     Build Number: ${TRAVIS_BUILD_NUMBER}\n
     Branch: ${TRAVIS_BRANCH}', 'channel': '${CHANNEL}'}" ${SLACK_ROOMS}
