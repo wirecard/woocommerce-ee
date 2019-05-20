738ad934abaebeb34046bbde67983ea5b9d5deef
brkicadis <adis.brkic@wirecard.com>
2019-05-20 11:13:39 +0200

    Test upload reports to chosen version 2

--- .bin/upload-logs-and-notify.sh
+++ .bin/upload-logs-and-notify.sh
@@ -25,10 +25,10 @@ DATE_FOLDER=${GATEWAY_FOLDER}/${TODAY}
 #if [ ! -d "${GATEWAY_FOLDER}" ]; then
 #mkdir ${GATEWAY_FOLDER}
 #fi
-#
-#if [ ! -d "${DATE_FOLDER}" ]; then
-#mkdir ${DATE_FOLDER}
-#fi
+
+if [ ! -d "${DATE_FOLDER}" ]; then
+mkdir ${DATE_FOLDER}
+fi
 
 #copy report files
 cp wirecard-woocommerce-extension/tests/_output/*.html ${DATE_FOLDER}
