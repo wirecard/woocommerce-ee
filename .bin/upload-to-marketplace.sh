e7a18dcacc5571bd93615a32a78ab23aed0407d0
Tatjana Starcenko <tatjana.starcenko@wirecard.com>
2019-06-14 14:20:09 +0200

    Update svn paths

--- .bin/upload-to-marketplace.sh
+++ .bin/upload-to-marketplace.sh
@@ -3,7 +3,7 @@
 WORDPRESS_DIR=wordpress
 PLUGIN_DIR=wirecard-woocommerce-extension
 VERSION=`jq .[0].release SHOPVERSIONS| tr -d \"`
-RELEASE_DIR=${WORDPRESS_DIR}/${PLUGIN_DIR}/tags/${VERSION}
+RELEASE_DIR=${WORDPRESS_DIR}/tags/${VERSION}
 
 composer install --no-dev
 
@@ -12,12 +12,12 @@ mkdir ${WORDPRESS_DIR}
 svn checkout https://plugins.svn.wordpress.org/wirecard-woocommerce-extension \
 	--username "${WORDPRESS_USER}" --password "${WORDPRESS_PASSWORD}"  -q ${WORDPRESS_DIR}
 cp -r ${PLUGIN_DIR}/{assets,classes,languages,vendor,readme.txt,woocommerce-wirecard-payment-gateway.php} \
-	${WORDPRESS_DIR}/${PLUGIN_DIR}/trunk
+	${WORDPRESS_DIR}/trunk
 
 mkdir ${RELEASE_DIR}
 cp -r ${PLUGIN_DIR}/{assets,classes,languages,vendor,readme.txt,woocommerce-wirecard-payment-gateway.php} ${RELEASE_DIR}
 
 cd ${WORDPRESS_DIR}
-svn add ${PLUGIN_DIR}/tags/${VERSION}/*
+svn add tags/${VERSION}/*
 svn commit -m "Add ${VERSION} release" --username "${WORDPRESS_USER}" --password "${WORDPRESS_PASSWORD}"
 echo "Successfully uploaded release to Wordpress"
