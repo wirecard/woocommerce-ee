a386ce1109f88e2535a1862a31b638a8609a0c13
Tatjana Starcenko <tatjana.starcenko@wirecard.com>
2019-06-14 11:35:46 +0200

    Add uploading to marketplace

---
+++ .bin/upload-to-marketplace.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+WORDPRESS_DIR=wordpress
+PLUGIN_DIR=wirecard-woocommerce-extension
+VERSION=`jq .[0].release SHOPVERSIONS`
+RELEASE_DIR= ${WORDPRESS_DIR}/tags/${VERSION}
+
+composer install --no-dev
+
+mkdir ${WORDPRESS_DIR}
+
+svn checkout https://plugins.svn.wordpress.org/wirecard-woocommerce-extension \
+	--username "${WORDPRESS_USER}" --password "${WORDPRESS_PASSWORD}" ${WORDPRESS_DIR}
+cp -r ${PLUGIN_DIR}/{assets,classes,languages, vendor,readme.txt,woocommerce-wirecard-payment-gateway.php} ${WORDPRESS_DIR}/trunk
+
+mkdir ${RELEASE_DIR}
+cp -r ${PLUGIN_DIR}/{assets,classes,languages, vendor,readme.txt,woocommerce-wirecard-payment-gateway.php} ${RELEASE_DIR}
+
+cd ${WORDPRESS_DIR}
+svn add tags/${VERSION}/*
+svn add trunk/*
+#svn commit -m "Add ${VERSION} release" --username "${WORDPRESS_USER}" --password "${WORDPRESS_PASSWORD}"
+echo "Successfully uploaded release to Wordpress"
+
+
