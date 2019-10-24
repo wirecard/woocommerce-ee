3a6d1ce707f266f9d63292ce161120b2a91a8fd6
Tatjana Starcenko <tatjana.starcenko@wirecard.com>
2019-10-24 12:23:29 +0200

    Update commit message on shopsystem update

--- .bin/get-woocommerce-versions.sh
+++ .bin/get-woocommerce-versions.sh
@@ -9,6 +9,6 @@ if [[ $(git diff HEAD ${WOOCOMMERCE_RELEASES_FILE}) != '' ]]; then
 	git config --global user.name "Travis CI"
 	git config --global user.email "wirecard@travis-ci.org"
 	git add  ${WOOCOMMERCE_RELEASES_FILE}
-	git commit -m "[skip ci] Update latest shop releases"
+	git commit -m "${SHOP_SYSTEM_UPDATE_COMMIT}"
 	git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master
 fi
