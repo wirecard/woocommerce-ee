a9369ad310502744547b1d78e2572b014691dbeb
Sebastijan Sakac <sebastijan.sakac@wirecard.com>
2020-01-20 13:33:19 +0100

    Change register_activation_hook function

--- wirecard-woocommerce-extension/Test/stubs/functions.php
+++ wirecard-woocommerce-extension/Test/stubs/functions.php
@@ -50,8 +50,7 @@ function load_plugin_textdomain() {
 }
 
 function register_activation_hook( $file, $function ) {
-	$file = plugin_basename( $file );
-	add_action( 'activate_' . $file, $function );
+	add_action( 'activate_' . plugin_basename( $file ), $function );
 }
 
 function add_action( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
