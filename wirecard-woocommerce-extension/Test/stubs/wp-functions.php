2e4f7a3a72b8767f86db994790ba9e69bed3e08b
Sebastijan Sakac <sebastijan.sakac@wirecard.com>
2020-01-20 14:00:38 +0100

    Revert "Move functions"
    This reverts commit a649c9dfd0c962c573e32516c9a151b4e9da2f61.

---
+++ wirecard-woocommerce-extension/Test/stubs/wp-functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function plugin_dir_path()
+{
+	return __DIR__ . '/../../';
+}
+
+function plugin_dir_url()
+{
+	return __DIR__ . '/../';
+}
+
+function plugin_basename()
+{
+	return 'wirecard-woocommerce-extension';
+}
