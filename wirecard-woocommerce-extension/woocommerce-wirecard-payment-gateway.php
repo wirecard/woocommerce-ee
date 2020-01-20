21818593d9ad552777f4b16ca8d089c3666b8f5a
Sebastijan Sakac <sebastijan.sakac@wirecard.com>
2020-01-20 11:32:38 +0100

    Testing unit test fix (#316)
    Update unit test

--- wirecard-woocommerce-extension/woocommerce-wirecard-payment-gateway.php
+++ wirecard-woocommerce-extension/woocommerce-wirecard-payment-gateway.php
@@ -47,15 +47,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WIRECARD_EXTENSION_NAME', 'Wirecard WooCommerce Extension' );
-define( 'WIRECARD_EXTENSION_VERSION', '3.0.1' );
-define( 'WIRECARD_EXTENSION_BASEDIR', plugin_dir_path( __FILE__ ) );
-define( 'WIRECARD_EXTENSION_URL', plugin_dir_url( __FILE__ ) );
-define( 'WIRECARD_EXTENSION_LOCALE_FALLBACK', 'en_US' );
-define( 'WIRECARD_EXTENSION_HEADER_PLUGIN_NAME', 'woocommerce-ee+Wirecard' );
-define( 'WIRECARD_EXTENSION_HELPER_DIR', WIRECARD_EXTENSION_BASEDIR . '/classes/helper/' );
-define( 'WIRECARD_EXTENSION_TEMPLATE_DIR', WIRECARD_EXTENSION_BASEDIR . '/templates/' );
-define( 'WIRECARD_EXTENSION_MAIN_FILE', plugin_basename( __FILE__ ) );
+require_once __DIR__ . '/woocommerce-wirecard-definitions.php';
 
 /**
  * Action that is triggered when a textdomain is loaded.
