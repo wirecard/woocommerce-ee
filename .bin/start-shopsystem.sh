d249c2a7cc20479843c06305048ffd804f959ee0
brkicadis <adis.brkic@wirecard.com>
2020-04-06 13:50:12 +0200

    Adapt script to use docker-compose

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -14,12 +14,11 @@ for ARGUMENT in "$@"; do
 done
 
 export WOOCOMMERCE_ADMIN_USER=admin
+export WOOCOMMERCE_ADMIN_PASSWORD=password
 
-docker-compose build --build-arg WOOCOMMERCE_VERSION="${WOOCOMMERCE_VERSION}" --build-arg GATEWAY="${GATEWAY}" webserver
+docker-compose build --build-arg WOOCOMMERCE_VERSION="${WOOCOMMERCE_VERSION}" web
 
-docker-compose up -d webserver
-docker-compose ps
-docker-compose up -d dbserver
+docker-compose up -d
 docker-compose ps
 
 # wordpress running on 9090
@@ -29,22 +28,22 @@ while ! $(curl --output /dev/null --silent --head --fail "${NGROK_URL}/wp-admin/
 done
 
 #install wordpress
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp core install --allow-root --url="${NGROK_URL}" --admin_password="${WOOCOMMERCE_DB_PASSWORD}" --title=test --admin_user=${WOOCOMMERCE_ADMIN_USER} --admin_email=test@test.com
+docker-compose exec wp core install --allow-root --url="${NGROK_URL}" --admin_password="${WOOCOMMERCE_ADMIN_PASSWORD}" --title=test --admin_user=${WOOCOMMERCE_ADMIN_USER} --admin_email=test@test.com
 
 #activate woocommerce
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp plugin activate woocommerce --allow-root
+docker-compose exec wp plugin activate woocommerce --allow-root
 
 #activate woocommerce-ee
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp plugin activate wirecard-woocommerce-extension --allow-root
+docker-compose exec wp plugin activate wirecard-woocommerce-extension --allow-root
 
 #install wordpress-importer
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp plugin install wordpress-importer --activate --allow-root
+docker-compose exec wp plugin install wordpress-importer --activate --allow-root
 
 #import sample product
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp import /var/www/html/wp-content/plugins/woocommerce/sample-data/sample_products.xml --allow-root --authors=create
+docker-compose exec wp import /var/www/html/wp-content/plugins/woocommerce/sample-data/sample_products.xml --allow-root --authors=create
 
 #activate storefront theme
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp theme install storefront --activate --allow-root
+docker-compose exec wp theme install storefront --activate --allow-root
 
 #install shop pages
-docker exec ${WOOCOMMERCE_CONTAINER_NAME} wp wc tool run install_pages --user=admin --allow-root
+docker-compose exec wp wc tool run install_pages --user=admin --allow-root
